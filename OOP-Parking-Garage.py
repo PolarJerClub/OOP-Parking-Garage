9b4515c40f94034ef0ed7b5d6bc4dfee1190f14e
Jeremy Kenney <jeremykenney.001@gmail.com>
2023-06-09 13:27:16 -0400

    final

--- OOP-Parking-Garage.py
+++ OOP-Parking-Garage.py
@@ -1,23 +1,3 @@
-"""
-Consider the following methods/ideas when constructing your garage: (these are suggestions to get you started, if you have other ideas that is totally cool and we love to see it!)
-
-take_ticket
-This should decrease the amount of tickets available by 1
-This should decrease the amount of parkingSpaces available by 1
-pay_for_parking
-Display an input that waits for an amount from the user and store it in a variable
-If the payment variable is not empty then (meaning the ticket has been paid) -> display a message to the user that their ticket has been paid and they have 15mins to leave
-This should update the "current_ticket" dictionary key "paid" to True 
-
-
--leave_garage
-If the ticket has been paid, display a message of "Thank You, have a nice day"
-If the ticket has not been paid, display an input prompt for payment
-Once paid, display message "Thank you, have a nice day!"
-Update parking_spaces list to increase by 1 (meaning add to the parking_spaces list)
-Update tickets list to increase by 1 (meaning add to the tickets list)
-"""
-
 class Garage(object):
     """
     OOP Parking Garage
@@ -36,6 +16,7 @@ class Garage(object):
         """
         Take the ticket.
         """
+        print("Vending ticket.")
         if self.parkingSpaces < self.max_spaces:
             self.tickets -= 1
             self.parkingSpaces -= 1
@@ -45,26 +26,39 @@ class Garage(object):
             print("The parking garage is full.")
 
     def pay_for_parking(self):
-        ticket_number = int(input("Enter your ticket number:"))
+        """
+        Pay for the parking ticket. 
+        """
+        print("Time to pay for parking.")
+        ticket_number = int(input("Enter your ticket number:")) # Enter the number of your parking ticket.
         if ticket_number in self.current_ticket and not self.current_ticket[ticket_number]["paid"]:
             payment = input("Enter the amount to pay: ")    
             if payment:
                 self.current_ticket[ticket_number]["paid"] = True
+                print("Your ticket has been paid. You have 15 mins to leave.")
         else:
-             print("Your ticket has been paid. You have 15 mins to leave.")
+             print("Not a valid ticket.")
             
     def leave_garage(self):
+        """
+        Leave the garage. Check the ticket number.
+        """
+        print("Leaving the garage.")
         ticket_number = int(input("Enter your ticket number:"))
-        if ticket_number in self.current_ticket:
+        if ticket_number not in self.current_ticket:
+            print("Not a valid ticket.")
+        while ticket_number in self.current_ticket:
             if self.current_ticket[ticket_number]["paid"] == True:
                 self.parkingSpaces -= 1
-                del self.current_ticket[ticket_number]
+                del self.current_ticket[ticket_number] # Get rid of the ticket from the dictionary.
                 print("Thank you, have a nice day!")
-                # Get rid of the ticket from the dictionary.
-        if self.current_ticket[ticket_number]["paid"] == False:
-            input("Please give your payment")
-        # elif self.current_ticket["paid"] = True:
-            print("Thank You, have a nice day!")
-            self.parkingSpaces += 1
+            else:
+                input("Please give your payment")
+                print("Thank You, have a nice day!")
+                del self.current_ticket[ticket_number] # Get rid of the ticket from the dictionary.
+                self.parkingSpaces += 1
 
-
+garage = Garage(5, 10, 12)
+garage.take_ticket()
+garage.pay_for_parking()
+garage.leave_garage()
